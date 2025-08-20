03301364bafcc88e3933e402daea664b0244ae62
dbailey1564 <137481635+dbailey1564@users.noreply.github.com>
2025-08-20 19:06:39 -0400

    feat: add player and house routes

---
+++ src/Player.tsx
@@ -0,0 +1,1 @@
+export default function Player() { return <div>Player page</div>; }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
+import Player from './Player'
+import House from './House'
 import Stats from './Stats'
 import './styles.css'
 
@@ -10,6 +12,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter basename="/roll-et">
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/player" element={<Player />} />
+        <Route path="/house" element={<House />} />
         <Route path="/stats" element={<Stats />} />
       </Routes>
     </BrowserRouter>
